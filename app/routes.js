511cdb435d5e2aafbee5e0e811ca0e5ea9ea6b93
Mohammad Asim Iqbal <mohammad.asim.iqbal.cs@gmail.com>
2023-03-13 15:08:14 +0000

    routes added

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,11 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.get('/homepage',(req,res)=> res.render('homepage'))
+router.get('/404',(req,res)=> res.render('404'))
+router.get('/500',(req,res)=> res.render('500'))
+router.get('/accessibility',(req,res)=> res.render('accessibility'))
+router.get('/cookies',(req,res)=> res.render('cookies'))
+router.get('/privacy',(req,res)=> res.render('privacy'))
+router.get('/roadmap',(req,res)=> res.render('roadmap'))
+router.get('/terms-and-conditions',(req,res)=> res.render('terms-and-conditions'))
