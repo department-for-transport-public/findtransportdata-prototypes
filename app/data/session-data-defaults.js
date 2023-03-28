8e1ff50c153c1e787cf7049a03896720da8287a8
Mohammad Asim Iqbal <mohammad.asim.iqbal.cs@gmail.com>
2023-03-28 14:05:19 +0100

    css and js added

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -2,4 +2,223 @@ module.exports = {
 
   // Insert values here
 
+  dashboard_dataset: [{
+    "author": "asim",
+    "author_email": "mohammad.asim.iqbal.cs@gmail.com",
+    "creator_user_id": "c29f300ce681",
+    "data_available": "no",
+    "data_formats": "",
+    "date_range_earliest_day": "",
+    "date_range_earliest_month": "",
+    "date_range_earliest_year": "",
+    "date_range_latest_day": "",
+    "date_range_latest_month": "",
+    "date_range_latest_year": "",
+    "end_date": "9999-12-31 00:00:00",
+    "id": "b4d2b53dc41a",
+    "isopen": false,
+    "license_id": "",
+    "license_title": "",
+    "locations": [
+      "East Midlands England"
+    ],
+    "maintainer": null,
+    "maintainer_email": null,
+    "metadata_created": "2023-03-08T09:23:32.752293",
+    "metadata_modified": "2023-03-08T09:23:32.752310",
+    "name": "new-dataset-186c08870b7",
+    "notes": "",
+    "num_resources": 0,
+    "num_tags": 5,
+    "organization": {
+      "id": "58dcc961-8138-4d22-b1e9-58e1b860342a",
+      "name": "basemap",
+      "title": "Basemap",
+      "type": "organization",
+      "description": "",
+      "image_url": "",
+      "created": "2021-03-12T16:43:12.632505",
+      "is_organization": true,
+      "approval_status": "approved",
+      "state": "active"
+    },
+    "owner_org": "58dcc961-8138-4d22-b1e9-58e1b860342a",
+    "private": false,
+    "regions": [
+      "Local Authority Districts - inc London Borough and Metropolitan Districts - and lower levels"
+    ],
+    "regularly_updated": "yes",
+    "regularly_updated_earliest_day": "2",
+    "regularly_updated_earliest_month": "10",
+    "regularly_updated_earliest_year": "2012",
+    "road_networks": [
+      "B roads"
+    ],
+    "start_date": "2012-10-02 00:00:00",
+    "state": "active",
+    "title": "New Dataset",
+    "topics": [
+      "Accidents and incidents"
+    ],
+    "transport_modes": [
+      "Bicycles"
+    ],
+    "type": "dataset",
+    "update_frequency": "live",
+    "url": "",
+    "version": "",
+    "tags": [
+      {
+        "display_name": "Accidents and incidents",
+        "id": "e0aa9b2f-dc7b-45a8-95bd-7327003a461c",
+        "name": "Accidents and incidents",
+        "state": "active",
+        "vocabulary_id": "9f39a98a-a137-4fe1-99d8-6e0ab2507f67"
+      },
+      {
+        "display_name": "B roads",
+        "id": "b9d1b287-f6f2-4b34-b356-83871d66df91",
+        "name": "B roads",
+        "state": "active",
+        "vocabulary_id": "9e8c99ad-5614-495d-8670-b6639d5c52f9"
+      },
+      {
+        "display_name": "Bicycles",
+        "id": "2e851fcc-2657-4a13-a02c-8edcca42fb7f",
+        "name": "Bicycles",
+        "state": "active",
+        "vocabulary_id": "ba841e48-70c7-4587-bd28-07b03608b97a"
+      },
+      {
+        "display_name": "East Midlands England",
+        "id": "988de6a0-1c9d-4de5-89c0-161c5062b1c4",
+        "name": "East Midlands England",
+        "state": "active",
+        "vocabulary_id": "cbdff1bd-580c-4c72-b655-32c24888cfa9"
+      },
+      {
+        "display_name": "Local Authority Districts - inc London Borough and Metropolitan Districts - and lower levels",
+        "id": "01fadf92-8399-4a74-a619-279ff8326ad0",
+        "name": "Local Authority Districts - inc London Borough and Metropolitan Districts - and lower levels",
+        "state": "active",
+        "vocabulary_id": "8c873dae-7012-45c1-a883-a7f28bc37a9b"
+      }
+    ],
+    "resources": [],
+    "groups": [],
+    "relationships_as_subject": [],
+    "relationships_as_object": []
+  }, 
+  
+  
+  {
+    "author": "asim",
+    "author_email": "mohammad.asim.iqbal.cs@gmail.com",
+    "creator_user_id": "c29f300ce681",
+    "data_available": "no",
+    "data_formats": "",
+    "date_range_earliest_day": "",
+    "date_range_earliest_month": "",
+    "date_range_earliest_year": "",
+    "date_range_latest_day": "",
+    "date_range_latest_month": "",
+    "date_range_latest_year": "",
+    "end_date": "9999-12-31 00:00:00",
+    "id": "2aa176be",
+    "isopen": false,
+    "license_id": "",
+    "license_title": "",
+    "locations": [
+      "East Midlands England"
+    ],
+    "maintainer": null,
+    "maintainer_email": null,
+    "metadata_created": "2023-03-08T09:23:32.752293",
+    "metadata_modified": "2023-03-08T09:23:32.752310",
+    "name": "new-dataset-186c08870b7",
+    "notes": "",
+    "num_resources": 0,
+    "num_tags": 5,
+    "organization": {
+      "id": "58dcc961-8138-4d22-b1e9-58e1b860342a",
+      "name": "basemap",
+      "title": "Basemap",
+      "type": "organization",
+      "description": "",
+      "image_url": "",
+      "created": "2021-03-12T16:43:12.632505",
+      "is_organization": true,
+      "approval_status": "approved",
+      "state": "active"
+    },
+    "owner_org": "58dcc961-8138-4d22-b1e9-58e1b860342a",
+    "private": false,
+    "regions": [
+      "Local Authority Districts - inc London Borough and Metropolitan Districts - and lower levels"
+    ],
+    "regularly_updated": "yes",
+    "regularly_updated_earliest_day": "2",
+    "regularly_updated_earliest_month": "10",
+    "regularly_updated_earliest_year": "2012",
+    "road_networks": [
+      "B roads"
+    ],
+    "start_date": "2012-10-02 00:00:00",
+    "state": "active",
+    "title": "New Dataset 1",
+    "topics": [
+      "Accidents and incidents"
+    ],
+    "transport_modes": [
+      "Bicycles"
+    ],
+    "type": "dataset",
+    "update_frequency": "live",
+    "url": "",
+    "version": "",
+    "tags": [
+      {
+        "display_name": "Accidents and incidents",
+        "id": "e0aa9b2f-dc7b-45a8-95bd-7327003a461c",
+        "name": "Accidents and incidents",
+        "state": "active",
+        "vocabulary_id": "9f39a98a-a137-4fe1-99d8-6e0ab2507f67"
+      },
+      {
+        "display_name": "B roads",
+        "id": "b9d1b287-f6f2-4b34-b356-83871d66df91",
+        "name": "B roads",
+        "state": "active",
+        "vocabulary_id": "9e8c99ad-5614-495d-8670-b6639d5c52f9"
+      },
+      {
+        "display_name": "Bicycles",
+        "id": "2e851fcc-2657-4a13-a02c-8edcca42fb7f",
+        "name": "Bicycles",
+        "state": "active",
+        "vocabulary_id": "ba841e48-70c7-4587-bd28-07b03608b97a"
+      },
+      {
+        "display_name": "East Midlands England",
+        "id": "988de6a0-1c9d-4de5-89c0-161c5062b1c4",
+        "name": "East Midlands England",
+        "state": "active",
+        "vocabulary_id": "cbdff1bd-580c-4c72-b655-32c24888cfa9"
+      },
+      {
+        "display_name": "Local Authority Districts - inc London Borough and Metropolitan Districts - and lower levels",
+        "id": "01fadf92-8399-4a74-a619-279ff8326ad0",
+        "name": "Local Authority Districts - inc London Borough and Metropolitan Districts - and lower levels",
+        "state": "active",
+        "vocabulary_id": "8c873dae-7012-45c1-a883-a7f28bc37a9b"
+      }
+    ],
+    "resources": [],
+    "groups": [],
+    "relationships_as_subject": [],
+    "relationships_as_object": []
+  } ]
+
 }
+
+
