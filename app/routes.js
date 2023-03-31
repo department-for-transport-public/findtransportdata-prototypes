8652f4385a55b0a96a83e23d13fe16fc2f82b79a
Mohammad Asim Iqbal <mohammad.asim.iqbal.cs@gmail.com>
2023-03-31 11:23:39 +0100

    admin dashboard prototype

--- app/routes.js
+++ app/routes.js
@@ -15,3 +15,5 @@ router.get('/cookies',(req,res)=> res.render('cookies'))
 router.get('/privacy',(req,res)=> res.render('privacy'))
 router.get('/roadmap',(req,res)=> res.render('roadmap'))
 router.get('/terms-and-conditions',(req,res)=> res.render('terms-and-conditions'))
+
+router.get('/admin/dashboard',(req,res)=> res.render('admin-dashboard'))
